30a596a04fac3fae06f693d702576518735a4082
Winbealle Baylon <winbeallebaylon8@gmail.com>
2025-06-02 20:16:05 +0800

    Updated repository configuration for GitHub Pages

--- Portfolio-master/src/App.js
+++ Portfolio-master/src/App.js
@@ -37,7 +37,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename="/MASULA-WEBSITE">
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
